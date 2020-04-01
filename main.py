176d9161af103d850da7cd8b4f8aee7218b8091c
datta07 <akula.gurudatta@gmail.com>
2020-04-02 00:50:36 +0530

    added live time

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return "datta welcomes you all guys"
+
+@app.route('/today')
+def today():
+	return time.strftime("%T")
