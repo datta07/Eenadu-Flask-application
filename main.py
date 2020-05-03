37d419dfe7560bc986d73cf07b4ebaec72b95813
datta07 <akula.gurudatta@gmail.com>
2020-05-03 15:54:23 +0530

    sending pdfs

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask import send_file,current_app
+import sak
 import time
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "datta Akula welcomes you all guys"
+	return "datta aula welcomes you all guys"
 
 @app.route('/today')
 def today():
 	return time.strftime("%T")
 
-@app.route('/show-pdf', methods=['GET', 'POST'])
-def download():
-	path = "k.pdf"
-	return send_file(path, as_attachment=True)
+@app.route('/sh-pdf/<flid>/<flname>', methods=['GET', 'POST'])
+def download(flid,flname):
+	sak.downloadPaper(flid)
+	path = "file.pdf"
+	return send_file(path,as_attachment=True,attachment_filename=flname+'.pdf')
 
 #app.run()
