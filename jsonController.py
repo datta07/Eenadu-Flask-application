4b34fc5322b4836c5b693e4827e30b334f91789a
datta07 <akula.gurudatta@gmail.com>
2020-05-04 21:46:19 +0530

    okr

--- jsonController.py
+++ jsonController.py
@@ -7,16 +7,18 @@ def set_values(date,value,link):
 		data[date][value]=link
 	else:
 		data[date]={value:link}
-	with open('data.json', 'w') as f:
+	with open('data.json','w') as f:
 		json.dump(data, f)
 
 def get_value(date,value):
-	data=json.loads(open("data.json","r").read())
+	with open("data.json","r") as f:
+		data=json.loads(f.read())
 	try:
 		return data[date][value]
 	except Exception:
 		return False
 
 def data():
-	data=json.loads(open("data.json","r").read())
+	with open("data.json","r") as f:
+		data=json.loads(f.read())
 	return str(data)
