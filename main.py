afa72cc744d8d8f5cd3f26db251cf790519c2a41
datta07 <akula.gurudatta@gmail.com>
2020-05-06 11:47:38 +0530

    added for telugu dialy

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def home():
 def today():
 	return time.strftime("%T")
 
-@app.route('/sh-pdf/<flid>/<flname>/<quality>', methods=['GET', 'POST'])
+@app.route('/santhi-pdf/<flid>/<flname>/<quality>', methods=['GET', 'POST'])
 def download(flid,flname,quality):
 	date=time.strftime("%d-%m-%Y")
 	link=fire.get_firebase(date,flid)
@@ -35,5 +35,18 @@ def download(flid,flname,quality):
 		fire.set_firebase(date,flid,link)
 		os.remove(flname+".pdf")
 		return "download "+flname+"<br>Download: <a href="+link+">clickHere</a><br>To Download other Papers from this app-><br><a href=https://play.google.com/store/apps/details?id=com.sdnews.epapers&hl=en>link</a>"
+
+@app.route('/Eenadu-pdfs/<flid>/<flname>/<quality>', methods=['GET', 'POST'])
+def download(flid,flname,quality):
+	date=time.strftime("%d-%m-%Y")
+	link=fire.get_firebase(date,flid)
+	if (link!=None):
+		return "download "+flname+"<br>Download: <a href="+link+">clickHere</a><br>To Download other Papers from this app-><br><a href=https://play.google.com/store/apps/details?id=com.telugudaily.downloader>link</a>"
+	else:
+		enaduNew.downloadPaper(flid,flname,quality)
+		link=dirve.getLink(flname+".pdf")
+		fire.set_firebase(date,flid,link)
+		os.remove(flname+".pdf")
+		return "download "+flname+"<br>Download: <a href="+link+">clickHere</a><br>To Download other Papers from this app-><br><a href=https://play.google.com/store/apps/details?id=com.telugudaily.downloader&hl=en>link</a>"
 
 #app.run()
